767916f2b850a13ed2a45efb0e5f1a4474ae0ce9
Sven König <sven@popmodernism.org>
2014-08-15 15:47:17 +0200

    gui: refactored, added lfos

--- gui.py
+++ gui.py
@@ -2,19 +2,18 @@
 
 import sys
 import time
+import threading
 import curses
+import time
+import math
 import numpy as np
 
-
 from model import middle_sorted as leds
-# leds = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-# leds = leds[:31]
 
 try:
 	from voxelspace_cython import VoxelSpace
 except ImportError:
 	from voxelspace import VoxelSpace
-
 
 from transformations import euler_matrix
 from math import radians as rad
@@ -29,17 +28,6 @@ def debug(*args):
 	for a in args:
 		sys.stderr.write(str(a) + ' ')
 	sys.stderr.write('\n\r')
-
-template_infopad = '''
-                   	a          w          c
-translate - X:%(tx)6.2f, Y:%(ty)6.2f, Z:%(tz)6.2f
-                    d          x          e
-
-                   r          f          v
-rotation  - X:  10.0°, Y:   0.0°, Z:   0.0°
-                   t          g          b
-'''
-
 
 class TitlePad(object):
 	def __init__(self):
@@ -62,142 +50,266 @@ class TitlePad(object):
 	def refresh(self):
 		self.pad.refresh(0,0,0,0,5,80)
 
+class TransformsWindow(object):
+	width = 80
+	height = 20
+
+	def __init__(self):
+		begin_x = 0; begin_y = 0
+		height = 11; width = 80
+		self.win = curses.newwin(height, width, begin_y, begin_x)
+
+	def update(self, transforms):
+		t = transforms
+		win = self.win
+
+		win.erase()
+
+		lines = [
+			'                  + d +      + w +      + e +',
+			'translation - X: %6.2f, Y: %6.2f, Z: %6.2f' % (t['tx'], t['ty'], t['tz']),
+			'                  - a -      - x -      - c -',
+			'',
+			'                  + t +      + g +      + b +',
+			'rotation    - X: %5.1f°, Y: %5.1f°, Z: %5.1f°' % (t['rx'], t['ry'], t['rz']),
+			'                  - r -      - f -      - v -',
+			'',
+			'                  + z +      + h +      + n +      + l +',
+			'scaling     - X: %5.1f%%, Y: %5.1f%%, Z: %5.1f%%,      XYZ' % (t['sx'], t['sy'], t['sz']),
+			'                  - u -      - j -      - m -      - k -',
+		]
+
+		for y, line in enumerate(lines):
+			win.addstr(y, 0, line)
+
+		win.refresh()
+
 
 class App(object):
 
-	def setup(self):
-		self.stdscr = curses.initscr()
-		curses.noecho()
-		curses.cbreak()
-		self.stdscr.keypad(1)
-
-	def exit(self):
-		self.stdscr.keypad(0)
-		curses.nocbreak()
-		curses.echo()
-		curses.endwin()
-
-	def start(self):
-		self.setup()
-		try:
-			self.run()
-			self.exit()
-		except:
-			self.exit()
-			raise
+	def start(self, *args, **kw):
+		def run(curses_window):
+			self.stdscr = curses_window
+			self.run(*args, **kw)
+		curses.wrapper(run)
 
 	def event_loop(self, funcs):
 		try:
-			while 1:
+			while not self._stop:
 				c = self.stdscr.getch()
-				info = str(c)
-				# debug(c, chr(c), c in funcs)
-				if c in funcs:
-					funcs[c]()
+				if c in funcs: funcs[c]()
 		except KeyboardInterrupt:
 			pass
 
-class Domulatrix(App):
+class Physics(threading.Thread):
+	def __init__(self, transforms, motion, update):
+		threading.Thread.__init__(self)
+		self.frame = -1
+		self.transforms = transforms
+		self.motion = motion
+		self.update = update
+		lp = self.lfo_pos = {}
+		lp['tx'] = lp['ty'] = lp['tz'] = 0
+		lp['sx'] = lp['sy'] = lp['sz'] = 0
+		lp['rx'] = lp['ry'] = lp['rz'] = 0
+
+		self.lfos = {
+			'rx': {'step': 0.01115, 'depth': 20},
+			'ry': {'step': 0.011711, 'depth': 30},
+			'rz': {'step': 0.012784, 'depth': 40},
+			'tz': {'step': 0.00283, 'depth': 130},
+			'sx': {'step': 0.00223, 'depth': 20},
+			'sy': {'step': 0.00223, 'depth': 20},
+			'sz': {'step': 0.00223, 'depth': 20},
+		}
+
+		self._stop = False
 
 	def run(self):
-		self.infopad = curses.newpad(20, 80)
+		while not self._stop:
+			time.sleep(0.02)
+			self.step()
+
+	def step(self):
+		self.frame += 1
+		t = self.transforms
+		m = self.motion
+		lp = self.lfo_pos
+
+		for attr in ['tx', 'ty', 'tz', 'rx', 'ry', 'rz']:
+			t[attr] += m[attr]
+
+		t = t.copy()
+		for attr, values in self.lfos.items():
+			step = values['step']
+			depth = values['depth']
+			lp[attr] += step 
+			t[attr] += math.sin(lp[attr])*depth
+
+		self.update(t)
+
+
+class Domulatrix(App):
+
+	def run(self, voxel_folder):
+		# titlepad = TitlePad()
+		# titlepad.refresh()
+
+		self._stop = False
+
+		self.voxels = VoxelSpace()
+		self.voxels.load(folder)
+
+		self.transforms_window = TransformsWindow()
 
 		class Transform(object): pass
 
-		t = self.transforms = Transform()
-		t.tx = 0
-		t.ty = 0
-		t.tz = 0
-		t.rx = 0
-		t.ry = 0
-		t.rz = 0
+		t = self.transforms = {}
+		t['tx'] = t['ty'] = t['tz'] = 0
+		t['sx'] = t['sy'] = t['sz'] = 100
+		t['rx'] = t['ry'] = t['rz'] = 0
 
-		self.update()
-
-		step_translate = 1
-		step_rotate    = 5
-
-		def tx_dec(): t.tx -= step_translate
-		def tx_inc(): t.tx += step_translate
-		def ty_dec(): t.ty -= step_translate
-		def ty_inc(): t.ty += step_translate
-		def tz_dec(): t.tz -= step_translate
-		def tz_inc(): t.tz += step_translate
-		def rx_dec(): t.rx = (t.rx - step_rotate) % 360
-		def rx_inc(): t.rx = (t.rx + step_rotate) % 360
-		def ry_dec(): t.ry = (t.ry - step_rotate) % 360
-		def ry_inc(): t.ry = (t.ry + step_rotate) % 360
-		def rz_dec(): t.rz = (t.rz - step_rotate) % 360
-		def rz_inc(): t.rz = (t.rz + step_rotate) % 360
-
-		def update_after(func):
-			def wrapped():
-				func()
-				self.update()
-			return wrapped
-
-		funcs = {
-			# translation
-			ord('a'): update_after(tx_dec),
-			ord('d'): update_after(tx_inc),
-			ord('w'): update_after(ty_dec),
-			ord('x'): update_after(ty_inc),
-			ord('c'): update_after(tz_dec),
-			ord('e'): update_after(tz_inc),
-			# rotation
-			ord('r'): update_after(rx_dec),
-			ord('t'): update_after(rx_inc),
-			ord('f'): update_after(ry_dec),
-			ord('g'): update_after(ry_inc),
-			ord('v'): update_after(rz_dec),
-			ord('b'): update_after(rz_inc),
+		m = self.motion = {}
+		m = self.motion = {
+			'tx': 0.03452,
+			'ty': 0.042525,
+			'tz': 0.047234,
+			'rx': 0,
+			'ry': 0,
+			'rz': 0,
 		}
 
-		self.event_loop(funcs)
+		self.update(t)
 
-	def update(self):
-		self.update_gui()
-		self.update_leds()
+		shift_multiplier = 6.
+		step_translate   = 1
+		step_scale       = 1.02
+		step_rotate      = 7.5
 
-	def update_gui(self):
-		t = self.transforms
+		xxx = t
 
-		txt = ''
-		txt =  'translate - X: %6.2f, Y: %6.2f, Z: %6.2f' % (t.tx, t.ty, t.tz)
-		txt += '\n\r'
-		txt += 'rotation  - X: %5.1f°, Y: %5.1f°, Z: %5.1f°' % (t.rx, t.ry, t.rz)
+		def sx_dec(m): xxx['sx'] /= step_scale ** m
+		def sx_inc(m): xxx['sx'] *= step_scale ** m
+		def sy_dec(m): xxx['sy'] /= step_scale ** m
+		def sy_inc(m): xxx['sy'] *= step_scale ** m
+		def sz_dec(m): xxx['sz'] /= step_scale ** m
+		def sz_inc(m): xxx['sz'] *= step_scale ** m
 
-		pad = self.infopad
+		def rx_dec(m): xxx['rx'] = (xxx['rx'] - step_rotate * m) % 360
+		def rx_inc(m): xxx['rx'] = (xxx['rx'] + step_rotate * m) % 360
+		def ry_dec(m): xxx['ry'] = (xxx['ry'] - step_rotate * m) % 360
+		def ry_inc(m): xxx['ry'] = (xxx['ry'] + step_rotate * m) % 360
+		def rz_dec(m): xxx['rz'] = (xxx['rz'] - step_rotate * m) % 360
+		def rz_inc(m): xxx['rz'] = (xxx['rz'] + step_rotate * m) % 360
 
-		pad.addstr(0, 0, '                + d +      + w +      + e +')
-		pad.addstr(1, 0, 'translate - X: %6.2f, Y: %6.2f, Z: %6.2f' % (t.tx, t.ty, t.tz))
-		pad.addstr(2, 0, '                - a -      - x -      - c -')
+		def event_wrapper(func, multiplier):
+			def wrapped():
+				func(multiplier)
+				self.update(self.transforms)
+			return wrapped
 
-		pad.addstr(3, 0, '')
+		def scale_dec(m):
+			xxx['sx'] /= step_scale ** m
+			xxx['sy'] /= step_scale ** m
+			xxx['sz'] /= step_scale ** m
 
-		pad.addstr(4, 0, '                + t +      + g +      + b +')
-		pad.addstr(5, 0, 'rotation  - X: %5.1f°, Y: %5.1f°, Z: %5.1f°' % (t.rx, t.ry, t.rz))
-		pad.addstr(6, 0, '                - r -      - f -      - v -')
+		def scale_inc(m):
+			xxx['sx'] *= step_scale ** m
+			xxx['sy'] *= step_scale ** m
+			xxx['sz'] *= step_scale ** m
 
-		pad.refresh(0,0,0,0,6,80)
+		def foo(*args):
+			self.physics._stop = True
+			self._stop = True
 
-	def get_transformed_model(self):
-		t = self.transforms
+		events = {
+
+			# translation
+			'a': {'attr': 'tx', 'func': lambda v,s,m: v-s*m, 'step': step_translate },
+			'd': {'attr': 'tx', 'func': lambda v,s,m: v+s*m, 'step': step_translate },
+			'w': {'attr': 'ty', 'func': lambda v,s,m: v-s*m, 'step': step_translate },
+			'x': {'attr': 'ty', 'func': lambda v,s,m: v+s*m, 'step': step_translate },
+			'c': {'attr': 'tz', 'func': lambda v,s,m: v-s*m, 'step': step_translate },
+			'e': {'attr': 'tz', 'func': lambda v,s,m: v+s*m, 'step': step_translate },
+
+			# scale
+			'z': sx_dec,
+			'u': sx_inc,
+			'h': sy_dec,
+			'j': sy_inc,
+			'n': sz_dec,
+			'm': sz_inc,
+			'k': scale_dec,
+			'l': scale_inc,
+
+			# rotation
+			'r': rx_dec,
+			't': rx_inc,
+			'f': ry_dec,
+			'g': ry_inc,
+			'v': rz_dec,
+			'b': rz_inc,			# translation
+			'a': {'attr': 'tx', 'func': lambda v,s,m: v-s*m, 'step': step_translate },
+			'd': {'attr': 'tx', 'func': lambda v,s,m: v+s*m, 'step': step_translate },
+			'w': {'attr': 'ty', 'func': lambda v,s,m: v-s*m, 'step': step_translate },
+			'x': {'attr': 'ty', 'func': lambda v,s,m: v+s*m, 'step': step_translate },
+			'c': {'attr': 'tz', 'func': lambda v,s,m: v-s*m, 'step': step_translate },
+			'e': {'attr': 'tz', 'func': lambda v,s,m: v+s*m, 'step': step_translate },
+
+			# special
+			'p': foo,
+		}
+
+		def create_hander(opts):
+			def handler(mul):
+				attr, func, step = opts['attr'], opts['func'], opts['step']
+				v = t[attr]
+				t[attr] = func(v, step, mul)
+
+			return handler
+
+
+		for char, opts in events.items():
+			if callable(opts): handler = opts
+			else:              handler = create_hander(opts)
+
+			events[ord(char)]         = event_wrapper(handler, 1.)
+			events[ord(char.upper())] = event_wrapper(handler, shift_multiplier)
+			del events[char]
+
+		self.physics = Physics(t, m, self.update)
+		self.physics.start()
+		self.event_loop(events)
+
+
+	def update(self, transforms):
+		self.transforms_window.update(transforms)
+		self.update_leds(transforms)
+
+	def get_transformed_model(self, transforms):
+		t = transforms
+
+		scaling_matrix = np.matrix([
+			[t['sx']/100, 0, 0, 1],
+			[0, t['sy']/100, 0, 1],
+			[0, 0, t['sz']/100, 1],
+			[0, 0, 0, 1]
+		])
 
 		translation_matrix = np.matrix([
-			[1, 0, 0, t.tx],
-			[0, 1, 0, t.ty],
-			[0, 0, 1, t.tz],
+			[1, 0, 0, t['tx']],
+			[0, 1, 0, t['ty']],
+			[0, 0, 1, t['tz']],
 			[0, 0, 0, 1   ]
 		])
 
 		rotation_matrix = np.matrix(euler_matrix(
-			rad(t.rx),
-			rad(t.ry),
-			rad(t.rz)
+			rad(t['rx']),
+			rad(t['ry']),
+			rad(t['rz'])
 		))
 
-		matrix = translation_matrix * rotation_matrix
+		matrix = scaling_matrix * translation_matrix * rotation_matrix
 
 		leds_ = leds.copy()
 		leds_ = np.pad(leds_, (0,1), 'constant', constant_values=1)[:-1]
@@ -208,11 +320,10 @@ class Domulatrix(App):
 
 		return leds_
 
-	def update_leds(self):
+	def update_leds(self, transforms):
 
-		leds = self.get_transformed_model()
-		colors = voxels.getpixels(leds)
-		colors = [list(rgb) for rgb in colors]
+		leds = self.get_transformed_model(transforms)
+		colors = self.voxels.getpixels(leds)
 
 		if strip is None: return
 
@@ -227,9 +338,5 @@ except IndexError:
 	print 'Argument missing.'
 	sys.exit()
 
-voxels = VoxelSpace()
-voxels.load(folder)
-
 app = Domulatrix()
-app.start()
-
+app.start(voxel_folder=folder)
